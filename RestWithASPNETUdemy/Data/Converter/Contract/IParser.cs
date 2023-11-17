a9692f75bc2e581985c16c12c54eb12676277cae
Ricardo da Cunha <joaovitorleitesantos98@gmail.com>
2023-11-16 21:05:43 -0300

    Add project files.

---
+++ RestWithASPNETUdemy/Data/Converter/Contract/IParser.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Data.Converter.Contract
+{
+    public interface IParser<O, D>
+    {
+        D Parse (O origin);
+
+        List<D> Parse(List<O> origin);
+    }
+}
